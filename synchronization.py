b7738c53dc759a7234afc4e31f547363f702bfa1
Nivia Andrade <129946160+niviadeandrade@users.noreply.github.com>
2023-12-21 22:48:26 +0000

    synchronization.py

--- synchronization.py
+++ synchronization.py
@@ -4,6 +4,13 @@ import time
 import hashlib
 
 def verify_folder (file_path):
-  hash = hashlib.md5()
+  hash_folder = hashlib.md5()
   with open (file_path, "rb") as fl:
+    for chunk in iter(lambda: fl.read(4096), b""):
+      hash_folder.update(chunk)
+  return hash_folder.hexdigest()
+
+
+
+
     
